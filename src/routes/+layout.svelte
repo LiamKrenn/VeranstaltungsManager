a22940574b3bf303e731527c801094c8ee214c98
LiamKrenn <k.liam1224@gmail.com>
2024-06-23 10:58:09 +0200

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { LogIn } from 'lucide-svelte';
 	import '../app.css';
 </script>
 
-<slot></slot>
-
-<style></style>
+<div class="flex h-screen flex-col">
+	<div class="flex h-16 items-center justify-between border-b-2 border-b-slate-700 px-3">
+		<h1 class="cursor-default text-3xl">Veranstaltungs Manager</h1>
+		<Button class="bg-slate-900" variant="outline">Log In <LogIn class="ml-2 p-0.5" /></Button>
+	</div>
+	<div class="w-full grow p-2">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-full w-full">
+	<div class="flex w-[30%] flex-col justify-between border-r-2 border-r-slate-700 p-2 pr-4 text-lg">
+		<div>
+			<h1 class="text-4xl font-bold mb-4">All Events In One Place</h1>
+			<p class="mb-4">Herzlich willkommen auf unserer Veranstaltungsplattform!</p>
+			<p>
+				Hier finden Sie eine Übersicht über alle kommenden Events. Entdecken Sie interessante
+				Veranstaltungen, melden Sie sich schnell und unkompliziert an und behalten Sie stets den
+				Überblick über Ihre Teilnahmen. Nutzen Sie unsere Plattform, um Ihre Anmeldungen zu
+				verwalten und bei Bedarf einfach zu stornieren. Starten Sie jetzt und verpassen Sie keine
+				Veranstaltung mehr!
+			</p>
+		</div>
+		<div>
+			<p>Sind Sie Veranstalter?</p>
+			<p class="mb-2">
+				Loggen Sie sich ein, oder kontaktieren sie uns unter: <a
+					class="underline"
+					href="mailto:admin@verman.at">admin@verman.at</a
+				>
+			</p>
+			<img class="w-full" src="/ver.png" alt="ver" />
+		</div>
+	</div>
+  <div class="w-full h-full">
+    <!-- Veranstaltungen -->
+  </div>
+</div>
